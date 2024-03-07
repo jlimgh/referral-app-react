31bff84e105583121c7d1a5422f841cf18e0783c
Jefferson Lim <jefferer@gmail>
2024-03-06 19:50:11 -0800

    [test deployment] for dev env

--- src/components/DashHeader.tsx
+++ src/components/DashHeader.tsx
@@ -154,7 +154,7 @@ const DashHeader = () => {
             <header className="dash-header">
                 <div className={`dash-header__container ${dashClass}`}>
                     <Link to="/dash">
-                        <h1 className="dash-header__title">Referral App</h1>
+                        <h1 className="dash-header__title">Referral App - Dev test</h1>
                     </Link>
                     <nav className="dash-header__nav">
                         {buttonContent}
